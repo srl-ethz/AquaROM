0f8415baceab76250fd070cc7b3db5cc6294a84f
Mathieu Dubied <mdubied@ethz.ch>
2026-02-05 17:54:59 +0100

    Nominal muscle volume for PROM

--- src/Actuation/reduced_tensors_actuation_PROM.m
+++ src/Actuation/reduced_tensors_actuation_PROM.m
@@ -14,6 +14,8 @@
 %   - actuationElements: elements subject to actuation
 %   - actuationDirection: a global direction in which the actuation takes
 %                         place
+%   - nomVolVector: [optional] vector of nominal volume of the elements,
+%                   used to multiply the force
 %
 % OUTPUT:
 %   tensors: a struct variable with the following fields:
@@ -29,11 +31,18 @@
 %   - List of currently supported elements: 
 %     TRI3
 %
-% Last modified: 06/03/2023, Mathieu Dubied, ETH Zürich
+% Last modified: 05/02/2026, Mathieu Dubied, ETH Zürich
 
-function tensors = reduced_tensors_actuation_PROM(myAssembly, V, U, actuationElements, actuationDirection)
+function tensors = reduced_tensors_actuation_PROM(myAssembly, V, U, actuationElements, actuationDirection, nomVolVector)
 
 t0=tic;
+
+% check
+if nargin < 6 || isempty(nomVolVector)
+    useNomVol = false;
+else
+    useNomVol = true;
+end
 
 % data from myAssembly
 nel      = myAssembly.Mesh.nElements;   % number of elements
@@ -45,18 +54,31 @@ RomAssembly = ReducedAssembly(myMesh, V);
 % compute reduced tensors
 disp(' REDUCED ACTUATION TENSORS (PROM):')
 fprintf(' Assembling %d elements ...\n', nel)
+if useNomVol
+    tic;
+    B1 = RomAssembly.vector_actuation('B1', 'weights', actuationElements, actuationDirection, nomVolVector);
+    fprintf('   B1: %.2f s\n',toc)
 
-tic;
-B1 = RomAssembly.vector_actuation('B1', 'weights', actuationElements, actuationDirection);
-fprintf('   B1: %.2f s\n',toc)
+    tic;
+    B2 = RomAssembly.matrix_actuation('B2', 'weights', actuationElements, actuationDirection, nomVolVector);
+    fprintf('   B2: %.2f s\n',toc)
 
-tic;
-B2 = RomAssembly.matrix_actuation('B2', 'weights', actuationElements, actuationDirection);
-fprintf('   B2: %.2f s\n',toc)
+    tic;
+    B3 = RomAssembly.matrix_actuation_PROM('B3', U, 'weights', actuationElements, actuationDirection, nomVolVector);
+    fprintf('   B3: %.2f s\n',toc)
+else
+    tic;
+    B1 = RomAssembly.vector_actuation('B1', 'weights', actuationElements, actuationDirection);
+    fprintf('   B1: %.2f s\n',toc)
 
-tic;
-B3 = RomAssembly.matrix_actuation_PROM('B3', U, 'weights', actuationElements, actuationDirection);
-fprintf('   B3: %.2f s\n',toc)
+    tic;
+    B2 = RomAssembly.matrix_actuation('B2', 'weights', actuationElements, actuationDirection);
+    fprintf('   B2: %.2f s\n',toc)
+
+    tic;
+    B3 = RomAssembly.matrix_actuation_PROM('B3', U, 'weights', actuationElements, actuationDirection);
+    fprintf('   B3: %.2f s\n',toc)
+end
 
 
 % display time needed for computation
